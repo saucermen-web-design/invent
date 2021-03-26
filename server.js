2b8d6f7efa476f3c1b59cee0c7a14e7da2e19283
cotymckinney <saucermenwebdesign@gmail.com>
2021-03-26 06:57:51 -0400

    reworks

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require('dotenv').config()
 
   // PROCCESS .ENV FILE
   const PORT = process.env.PORT || 3001;
-  const MONGODB_URI = toString(process.env.MONGODB_URI);
+  const MONGODB_URI = process.env.MONGODB_URI;
 
 // Load up mongoose
   const mongoose = require("mongoose");

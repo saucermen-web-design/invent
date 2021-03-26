7808a6ef2b8b58fded29e0f5218fafb9e33403e9
cotymckinney <saucermenwebdesign@gmail.com>
2021-03-26 10:29:14 -0400

    post presentation commit

--- models/item.js
+++ models/item.js
@@ -1,19 +1,19 @@
-const { urlencoded } = require("express");
-const mongoose = require("mongoose");
+  const { urlencoded } = require("express");
+  const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
+  const Schema = mongoose.Schema;
 
-const itemSchema = new Schema({
-  type: { type: String, required: true },
-  name: { type: String, required: true },
-  description: String,
-  condition: Number,
-  count: { type: String, required: true },
-  forSale: Boolean,
-  listing: String,
-  dateAdded: Date,
-});
+  const itemSchema = new Schema({
+    type: { type: String, required: true },
+    name: { type: String, required: true },
+    description: String,
+    condition: Number,
+    count: { type: String, required: true },
+    forSale: Boolean,
+    listing: String,
+    dateAdded: Date,
+  });
 
-const Item = mongoose.model("Item", itemSchema);
+  const Item = mongoose.model("Item", itemSchema);
 
-module.exports = Item;
+  module.exports = Item;

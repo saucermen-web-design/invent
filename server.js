6d7e096c8df14e7d5b85459703bff7afe0d19f5d
cotymckinney <saucermenwebdesign@gmail.com>
2021-03-26 07:34:37 -0400

    changes to mongoUrl for heroku troubleshooting

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ require('dotenv').config()
         secure: true,
         maxAge:60000
            },
-    store: MongoStore.create({ client: MONGODB_URI, dbName: 'projectWeek2' }), 
+    store: MongoStore.create({ mongoUrl: MONGODB_URI, dbName: 'projectWeek2' }), 
     secret: 'supersecret',
     saveUninitialized: true,
     resave: false

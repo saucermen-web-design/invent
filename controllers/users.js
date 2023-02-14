f4dc0d8b3b7a498b19a463de9e5e60c18045911f
Coty McKinney <cmckin8741@gmail.com>
2023-02-13 23:12:19 -0500

    mysql migration for user model and controller completed

--- controllers/users.js
+++ controllers/users.js
@@ -1,65 +1,73 @@
 // REQUIRED MODULES
-    const express = require('express');
-    const router = express.Router();
-    const { createUser, pool } = require('../models/user');
-    const bcrypt = require('bcrypt');
-    const SALT_ROUNDS = 10;
+const express = require('express');
+const router = express.Router();
+const { createUser, pool } = require('../models/user');
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 10;
 
-// 
-    router.get('/new', (req, res) => {
-        res.render('users/new');
-    });
+// NEW USER FORM
+router.get('/new', (req, res) => {
+  res.render('users/new');
+});
 
-    router.post('/signup', async (req, res) => {
-        const { username, password, email } = req.body;
+// CREATE NEW USER
+router.post('/signup', async (req, res) => {
+  const { username, password, email } = req.body;
 
-        try {
-            const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
-        await createUser(username, hashedPassword, email);
-            res.redirect('/login');
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ message: 'Internal server error' });
-        }
-    });
+  try {
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+    await createUser(username, hashedPassword, email);
+    res.redirect('/login');
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
-    router.get('/login', (req, res) => {
-        res.render('users/login');
-    });
+// LOGIN FORM
+router.get('/login', (req, res) => {
+  res.render('users/login');
+});
 
-    router.post('/login', (req, res) => {
-        const { username, password } = req.body;
+// LOGIN USER
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
 
-        pool.query('SELECT * FROM users WHERE username = ?', [username], async (error, results) => {
-            if (error) {
-                console.error(error);
-                res.status(500).json({ message: 'Internal server error' });
-            } else if (results.length === 0) {
-                res.redirect('/users/login');
-            } else {
-                const { id, username, password: hashedPassword, email } = results[0];
-                const doesPasswordMatch = await bcrypt.compare(password, hashedPassword);
-                if (doesPasswordMatch) {
-                    req.session.userId = id;
-                    res.redirect('/items');
-                } else {
-                    res.redirect('/users/login');
-                }
-            }
-        });
-    });
+  try {
+    const [rows] = await pool.execute('SELECT * FROM users WHERE username = ? LIMIT 1', [username]);
 
-    router.get('/dashboard', (req, res) => {
-        if (req.session.userId) {
-            res.render('items/index');
-        } else {
-            res.redirect('/users/login');
-        }
-    });
+    if (rows.length === 0) {
+      res.redirect('/users/login');
+    } else {
+      const { id, username, password: hashedPassword, email } = rows[0];
+      const doesPasswordMatch = await bcrypt.compare(password, hashedPassword);
 
-    router.get('/logout', (req, res) => {
-        req.session.destroy();
-        res.redirect('/');
-    });
+      if (doesPasswordMatch) {
+        req.session.userId = id;
+        res.redirect('/items');
+      } else {
+        res.redirect('/users/login');
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
-    module.exports = router;
+// DASHBOARD
+router.get('/dashboard', (req, res) => {
+  if (req.session.userId) {
+    res.render('items/index');
+  } else {
+    res.redirect('/users/login');
+  }
+});
+
+// LOGOUT
+router.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,61 +1,38 @@
-// REQUIRED MODULES
-    const dotenv = require('dotenv');
-    const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+const mysql = require('mysql2/promise');
 
-// LOAD ENVIRONMENT VARIABLES
-    dotenv.config();
-    const PORT = process.env.PORT || 3001;
-    const MYSQL_HOST = process.env.MYSQL_HOST;
-    const MYSQL_USER = process.env.MYSQL_USER;
-    const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD;
-    const MYSQL_DATABASE = process.env.MYSQL_DATABASE;
+dotenv.config();
 
-// CREATE CONNECTION POOL
-    const pool = mysql.createPool({
-        host: MYSQL_HOST,
-        user: MYSQL_USER,
-        password: MYSQL_PASSWORD,
-        database: MYSQL_DATABASE,
-        waitForConnections: true,
-        connectionLimit: 10,
-        queueLimit: 0,
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-    });
-    
-// DEFINE SCHEMA
-    const userSchema = {
-        username: 'VARCHAR(255)',
-        password: 'VARCHAR(255)',
-        email: 'VARCHAR(255)',
-        createdAt: 'DATETIME',
-        updatedAt: 'DATETIME',
+async function createUsersTable() {
+  const conn = await pool.getConnection();
+  await conn.query(`CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    createdAt DATETIME,
+    updatedAt DATETIME,
+    PRIMARY KEY (id)
+  )`);
+  conn.release();
+}
 
-    };
+createUsersTable();
 
-// CREATE USERS TABLE IS IT DOES NOT EXIST
-    async function createUsersTable() {
-        const conn = await pool.getConnection();
-        await conn.query(`CREATE TABLE IF NOT EXISTS users (
-            id INT NOT NULL AUTO_INCREMENT,
-            username VARCHAR(255) NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            email VARCHAR(255) NOT NULL,
-            createdAt DATETIME,
-            updatedAt DATETIME,
-            PRIMARY KEY (id)
-        )`);
-        conn.release();
-    }
-  
+async function createUser(username, password, email) {
+  const conn = await pool.getConnection();
+  await conn.query('INSERT INTO users (username, password, email, createdAt, updatedAt) VALUES (?, ?, ?, NOW(), NOW())', [username, password, email]);
+  conn.release();
+}
 
-    createUsersTable();
-
-// CREATE NEW USER
-    async function createUser(username, password, email) {
-        const conn = await pool.getConnection();
-        await conn.query('INSERT INTO users (username, password, email, createdAt, updatedAt) VALUES (?, ?, ?, NOW(), NOW())', [username, password, email]);
-        conn.release();
-    }
-
-// EXPORT
-    module.exports = { createUser, pool };
+module.exports = { createUser, pool };
